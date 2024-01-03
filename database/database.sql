8d353987149d7f95254752ee3b4b0aa08fe2b8e3
Ernesto Guerrero <ernestojair2020@gmail.com>
2024-01-03 17:45:31 -0500

    Cambios

--- database/database.sql
+++ database/database.sql
@@ -25,10 +25,7 @@ CREATE TABLE IF NOT EXISTS `correos` (
   UNIQUE KEY `correos_email_unique` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla ventas.correos: ~2 rows (aproximadamente)
-INSERT INTO `correos` (`id`, `nombre`, `email`, `created_at`, `updated_at`) VALUES
-	('0a40012a-6d8f-463f-afdb-35f8387c8f96', 'Zad', 'zading2023@gmail.com', '2023-12-25 01:19:01', '2023-12-25 01:19:01'),
-	('545f5a78-2f4f-41da-9293-b38f0b6b23a9', 'Jair Guerrero', 'ernestojair2023@outlook.es', '2023-12-25 01:15:07', '2023-12-25 01:15:07');
+-- Volcando datos para la tabla ventas.correos: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla ventas.dashboards
 CREATE TABLE IF NOT EXISTS `dashboards` (
@@ -61,22 +58,22 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla ventas.migrations: ~12 rows (aproximadamente)
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(20, '2023_12_16_193342_create_ventas_table', 1),
-	(43, '2014_10_12_000000_create_users_table', 2),
-	(44, '2014_10_12_100000_create_password_reset_tokens_table', 2),
-	(45, '2019_05_03_000001_create_customer_columns', 2),
-	(46, '2019_05_03_000002_create_subscriptions_table', 2),
-	(47, '2019_05_03_000003_create_subscription_items_table', 2),
-	(48, '2019_08_19_000000_create_failed_jobs_table', 2),
-	(49, '2019_12_14_000001_create_personal_access_tokens_table', 2),
-	(50, '2023_12_13_200441_create_dashboards_table', 2),
-	(51, '2023_12_13_200455_create_correos_table', 2),
-	(52, '2023_12_13_200511_create_promociones_table', 2),
-	(53, '2023_12_14_000831_create_promo_vendidos_table', 2);
+	(55, '2014_10_12_000000_create_users_table', 2),
+	(56, '2014_10_12_100000_create_password_reset_tokens_table', 2),
+	(57, '2019_05_03_000001_create_customer_columns', 2),
+	(58, '2019_05_03_000002_create_subscriptions_table', 2),
+	(59, '2019_05_03_000003_create_subscription_items_table', 2),
+	(60, '2019_08_19_000000_create_failed_jobs_table', 2),
+	(61, '2019_12_14_000001_create_personal_access_tokens_table', 2),
+	(62, '2023_12_13_200441_create_dashboards_table', 2),
+	(63, '2023_12_13_200455_create_correos_table', 2),
+	(64, '2023_12_13_200511_create_promociones_table', 2),
+	(65, '2023_12_14_000831_create_promo_vendidos_table', 2);
 
 -- Volcando estructura para tabla ventas.password_reset_tokens
 CREATE TABLE IF NOT EXISTS `password_reset_tokens` (
@@ -121,9 +118,7 @@ CREATE TABLE IF NOT EXISTS `promociones` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla ventas.promociones: ~1 rows (aproximadamente)
-INSERT INTO `promociones` (`id`, `nombre_paquete`, `descripcion_paquete`, `costo_adulto`, `costo_ninio`, `imagen`, `promocion`, `created_at`, `updated_at`) VALUES
-	('8173a8a8-ba69-41c6-afc1-e4e7dbb825bd', 'Bebidas alcoholicas', 'Las bebidas son algo que se deben tomar con precaución por que no e spra todos...', 1000, 200, 'storage/imagenes/ZZbao2l1NPuc45PydlGZoajrMjFx7CSFfc7Y1BnF.jpg', 0, '2023-12-24 23:38:32', '2023-12-24 23:38:32');
+-- Volcando datos para la tabla ventas.promociones: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla ventas.promo_vendidos
 CREATE TABLE IF NOT EXISTS `promo_vendidos` (
@@ -134,34 +129,14 @@ CREATE TABLE IF NOT EXISTS `promo_vendidos` (
   `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `cantidad_adultos` int NOT NULL,
   `cantidad_ninio` int NOT NULL,
+  `cantidad_dias` int NOT NULL,
   `total` double NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla ventas.promo_vendidos: ~20 rows (aproximadamente)
-INSERT INTO `promo_vendidos` (`id`, `nombre_paquete`, `telefono`, `correo`, `nombre`, `cantidad_adultos`, `cantidad_ninio`, `total`, `created_at`, `updated_at`) VALUES
-	('1e628aa9-00d6-45b0-86f5-c4f90d6be582', 'Bebidas alcoholicas', '0986496051', 'ernestojair2023@outlook.es', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 00:42:36', '2023-12-25 00:42:36'),
-	('2cf2a8a2-d5bf-42d1-a645-5cd8549c8e31', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 00:07:20', '2023-12-25 00:07:20'),
-	('30fb69b5-9f35-43f0-99db-97a47ed7aff6', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 00:33:29', '2023-12-25 00:33:29'),
-	('33f3da1b-3027-4f4f-a3a4-7f381859ebb9', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 00:14:47', '2023-12-25 00:14:47'),
-	('37e6f462-0af5-4060-8c3f-d186680c385f', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Guerrero', 4, 4, 4800, '2023-12-25 00:16:05', '2023-12-25 00:16:05'),
-	('3b609b03-5f49-482e-a79f-d2b10113cb03', 'Bebidas alcoholicas', '0986496051', 'ernesto2023guerrero@outlook.com', 'Wilther Guerrero', 4, 4, 4800, '2023-12-25 00:27:53', '2023-12-25 00:27:53'),
-	('4a6ed8d9-61e2-4ecb-9663-f0026a016dda', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Guerrero', 4, 4, 4800, '2023-12-25 00:09:46', '2023-12-25 00:09:46'),
-	('669c8cd8-c537-4373-9d04-48e58f0220cd', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 00:08:56', '2023-12-25 00:08:56'),
-	('672ad987-55b5-4735-bf24-117a4c791a67', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 01:32:15', '2023-12-25 01:32:15'),
-	('725b7d94-23bc-4c41-b3c1-67246a12fe94', 'Bebidas alcoholicas', '0986496051', 'ernestojair2023@outlook.es', 'Ernesto Jair', 4, 2, 4400, '2023-12-25 00:47:54', '2023-12-25 00:47:54'),
-	('84f59f17-c5c6-4dfc-8a84-8b9726ed51d2', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 01:29:02', '2023-12-25 01:29:02'),
-	('8817283e-728e-4446-95d0-9bfe0c805127', 'Bebidas alcoholicas', '0986496051', 'ernestojair2023@outlook.es', 'Ernesto Guerrero', 4, 2, 4400, '2023-12-25 00:36:20', '2023-12-25 00:36:20'),
-	('938ff41e-7570-442b-beaa-99a433efd0bb', 'Bebidas alcoholicas', '0986496051', 'aj244117c@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 01:24:46', '2023-12-25 01:24:46'),
-	('9c72f42a-6b75-4920-9af7-fa64f39dfc5a', 'Bebidas alcoholicas', '0986496054', 'ernestojair2020@gmail.com', 'Hola', 4, 2, 4400, '2023-12-24 23:42:34', '2023-12-24 23:42:34'),
-	('9e54e0f6-e1c3-47f0-bc95-cae7d163cd2c', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Guerrero', 5, 4, 5800, '2023-12-24 23:39:31', '2023-12-24 23:39:31'),
-	('afe20bcf-a3e5-4ec2-b0c7-a4297402db50', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 00:28:35', '2023-12-25 00:28:35'),
-	('b33c98fa-55bf-4353-a677-efe56a1db8c4', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Guerrero', 4, 4, 4800, '2023-12-25 00:19:10', '2023-12-25 00:19:10'),
-	('b73022f3-fd76-47c2-8219-0a9379c9c276', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 00:04:39', '2023-12-25 00:04:39'),
-	('bb55d1ed-16f7-4134-ab78-a81734edda56', 'Bebidas alcoholicas', '0986496051', 'ernestojair2020@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 01:33:23', '2023-12-25 01:33:23'),
-	('ff7d251e-5aac-4edc-b183-9a50c92dc4a6', 'Bebidas alcoholicas', '0986496051', 'aj244117c@gmail.com', 'Ernesto Jair', 4, 4, 4800, '2023-12-25 01:22:42', '2023-12-25 01:22:42');
+-- Volcando datos para la tabla ventas.promo_vendidos: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla ventas.subscriptions
 CREATE TABLE IF NOT EXISTS `subscriptions` (
@@ -221,7 +196,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 -- Volcando datos para la tabla ventas.users: ~1 rows (aproximadamente)
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `stripe_id`, `pm_type`, `pm_last_four`, `trial_ends_at`) VALUES
-	(1, 'Ernesto Jair', 'ernestojair2020@gmail.com', NULL, '$2y$12$tXeLvrP.QI0lwbvy76r7ouspeLKVRYS.NbrjLZB0nYbMN4/WIJJIe', NULL, '2023-12-24 23:37:19', '2023-12-24 23:37:19', NULL, NULL, NULL, NULL);
+	(1, 'Usuario  Prueba', 'userprueba@gmail.com', NULL, '$2y$12$SwHNN.vnPrcl.yWllhozaeKlpoa9hPgelRvYALdA/R/DJoayVc4Ty', NULL, '2023-12-26 17:53:07', '2023-12-26 17:53:07', NULL, NULL, NULL, NULL);
 
 -- Volcando estructura para tabla ventas.ventas
 CREATE TABLE IF NOT EXISTS `ventas` (
--- resources/views/admin/correos/correos.blade.php
+++ resources/views/admin/correos/correos.blade.php
@@ -3,20 +3,30 @@
 @section('main')
     <div class="p-4 sm:ml-64">
         <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-
-            <nav class="flex mb-5" aria-label="Breadcrumb">
-                <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                    <li class="inline-flex items-center">
-                        <a
-                            class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                            <span class="font-black text-3xl">
-                                <i class="fas fa-envelope text-blue-500"></i>
-                                <span class="text-blue-500">Correos</span>
-                            </span>
-                        </a>
-                    </li>
-                </ol>
-            </nav>
+            <div class="flex justify-between items-center">
+                <div class="w-1/2">
+                    <nav class="flex mb-5" aria-label="Breadcrumb">
+                        <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                            <li class="inline-flex items-center">
+                                <a
+                                    class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                                    <span class="font-black text-3xl">
+                                        <i class="fas fa-envelope text-blue-500"></i>
+                                        <span class="text-blue-500">Correos</span>
+                                    </span>
+                                </a>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+                <div class="w-1/2 text-right">
+                    <button data-modal-target="default-modal" data-modal-toggle="default-modal"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        type="button">
+                        <i class="fas fa-plus mr-2"></i> Nuevo
+                    </button>
+                </div>
+            </div>
 
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
